d11a7e06d837fd28332a79d273987781a0d49464
Dmitry Shvetsov <shvetsovdm@gmail.com>
2021-06-06 18:53:17 +1000

    feat: scroll to the top and bottom

--- content.js
+++ content.js
@@ -14,12 +14,18 @@ class FastPage {
   move(xPixel, yPixel) {
     this._window.scrollBy(xPixel, yPixel)
   }
+
+  scrollTo(opts) {
+    this._window.scrollTo(opts)
+  }
 }
 
 const keyBindings = {
   BINDING: Object.freeze({
     KeyJ: { name: 'move', args: [0, 50] },
-    KeyK: { name: 'move', args: [0, -50] }
+    KeyK: { name: 'move', args: [0, -50] },
+    KeyG: { name: 'scrollTo', args: [{ top: 0, behavior: 'smooth' }] },
+    KeyGShift: { name: 'scrollTo', args: [() => ({ top: document.body.scrollHeight, behavior: 'smooth' })] }
   }),
 
   NO_BINDING: Object.freeze({ name: 'idle', args: [] }),
@@ -27,6 +33,13 @@ const keyBindings = {
   find(code) {
     return keyBindings.BINDING[code] || keyBindings.NO_BINDING
   }
+}
+
+const isFunction = (value) => typeof value === 'function'
+const isUserInteractsWithForm = (event) => typeof event.target.type !== 'undefined'
+const isUserInteractWithContentEditable = (event) => event.target.contentEditable === 'true'
+
+const findCommandFromEvent = (event) => {
 }
 
 const applyCommandToPage = (command) => {
@@ -38,8 +51,12 @@ const applyCommandToPage = (command) => {
   return false
 }
 
-const isUserInteractsWithForm = (event) => typeof event.target.type !== 'undefined'
-const isUserInteractWithContentEditable = (event) => event.target.contentEditable === 'true'
+const unwrapDynamicFunctions = (command) => {
+  return ({
+    ...command,
+    args: command.args.map((commandArg) => isFunction(commandArg) ? commandArg() : commandArg)
+  })
+}
 
 const keyDownHandler = (event) => {
   debug.verbose(`key down with code [${event.code}] received`)
@@ -47,11 +64,16 @@ const keyDownHandler = (event) => {
     return
   }
 
-  const command = keyBindings.find(event.code)
-  if (applyCommandToPage(command)) {
-    debug.verbose(`key down with code [${event.code}] that corresponds to command [${command.name}] with arguments [${command.args}] handled`)
+  commandKeyParts = [event.code]
+  if (event.shiftKey) {
+    commandKeyParts.push('Shift')
+  }
+  const commandKey = commandKeyParts.join('')
+  const command = keyBindings.find(commandKey)
+  if (applyCommandToPage(unwrapDynamicFunctions(command))) {
+    debug.verbose(`key down with code [${commandKey}] that corresponds to command [${command.name}] with arguments [${command.args}] handled`)
   } else {
-    debug.verbose(`page no [${event.code}] that corresponds to command [${command.name}] has no support on page object`)
+    debug.verbose(`page no [${commandKey}] that corresponds to command [${command.name}] has no support on page object`)
   }
 }
 
